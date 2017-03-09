cb1dc7a283908f8118358be6e036695b54e119f0
Michael Habib <michaelhabib1868@gmail.com>
2017-03-09 11:22:54 -0800

    Send keyword input to api gateway to trigger lambda

--- config.js
+++ config.js
@@ -3,5 +3,11 @@ const env = process.env;
 export default {
   port: env.PORT || 3000,
   host: env.HOST || '0.0.0.0',
+  https_port:443,
+  baseURL:'https://7yy7fqe0fl.execute-api.us-east-1.amazonaws.com',
+  url:'/KeywordStage/keyword',
+  headers:{
+    'Content-Type':'application/json'
+  }
  
 };
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import config from './config';
 import bodyParser from 'body-parser';
+import axios from 'axios';
+import morgan from 'morgan'
+import https from 'https';
 
 const server = express();
 
@@ -8,6 +11,9 @@ const server = express();
 server.use(express.static('static'));
 server.use(express.static('node_modules/bootstrap/dist'));
 server.use(express.static('node_modules/font-awesome'))
+
+//middleware to log requests
+server.use(morgan('tiny'));
 
 //force middleware to use body-parser
 server.use(bodyParser.json());
@@ -18,13 +24,40 @@ server.use(bodyParser.urlencoded({
 server.get('/',(req,res)=>{
     res.sendFile(__dirname+'/public/index.html');
 });
+ 
+let host =  '7yy7fqe0fl.execute-api.us-east-1.amazonaws.com';
+let port = 443;
+var options = {
+    host:host,
+    path:'/KeywordStage/keyword',
+    port:port,
+    headers:{
+        'Content-Type':'application/json'
+    },
+    method:'POST'
+};
 
-server.post('/',(req,res)=>{
-    debugger;
+
+
+server.post('/charts.html',(req,res)=>{
+    axios.request({
+        method:'post',
+        baseURL:config.baseURL,
+        url:config.url,
+        headers:config.headers,
+        data:{
+            keyword:req.body.keyword
+        }
+    }).then((res)=>{
+        console.log(res.data);
+
+    }).catch((e)=>{
+        console.log(e);
+    });
     const keyword = req.body.keyword;
     res.sendFile(__dirname+'/public/charts.html');
 
-})
+});
 
 
 server.listen(config.port,config.host,()=>{
